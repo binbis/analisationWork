608a3cb5d5f7ae19da3e9dbebc57810b8f43d110
opium <67838661+binbis@users.noreply.github.com>
2024-06-28 20:53:11 +0300

    fix bag with spaces

--- fastCopDelta_V2.cs
+++ fastCopDelta_V2.cs
@@ -1,49 +1,48 @@
 /**
-24,06,2024_v2.1 
+24,06,2024_v2.2
 0. відкрий дельту в окремому вікні, не міняй вкладку, (можеш звернути це вікно)
 1. виділяєш текст де є координати mgrs
 2. скрипт копіює їх, прибирає та виправляє їх,
 3. відкриває дельту(де б вона не була та який розмір вікна б не мала) 
 4. вставяє оброблені координати до пошуку та натискає кнопку пошуку
+
+Завдання:
+1. ще не придумав як нормально заміняти укр-ру букри на англ.
+2. можливо потрібно змінити концет
 */
+//using System;
 using System.Windows.Forms;
+//using System.Text.RegularExpressions;
 namespace CSLight
 {
-    internal class Program
+    class Program
     {
         [STAThread]
-        static void Main(string[] args)
+        static void Main()
         {
 			opt.mouse.MoveSpeed = opt.key.KeySpeed = opt.key.TextSpeed = 10;
-			//копіюємо код
-			keys.send("Ctrl+C");			
-			// Зчитуємо вміст з буферу обміну
-			string clipText = Clipboard.GetText();
-		
+			string clipText = "Дата/час виявлення - 24.06.2024 19:07 Координати - 37U Cr 52470 81419 Екіпаж - Домаха 1; Ціль - 2 квадроцикла Дод.коментарі - до КПВВ";
+			//string patternFirstEnglish = @"[a-zA-Z]"; // шаблон (3) 1 англ буква
 			string patternFirstEnglishBefore = @".{0,2}[a-zA-Z]"; // шаблон (1) 1 англ + 2 символи попереду 
-			string patternFirstEnglishAfter = @"[a-zA-Z].{0,15}"; // шаблон (2) 1 англ + 15 символів після (з запасом)
+			string patternFirstEnglishAfter = @"[a-zA-Z].{0,12}"; // шаблон (2) 1 англ + 15 символів після (з запасом)
 			string firstEnglishBefore = string.Empty;
 			string firstEnglishAfter = string.Empty;
 			
-			// Видаляємо все окрім цифр та англ букв, переводим в один регістр
+			// приводимо до онго регістру
 			clipText = clipText.ToUpper();
-			// заміняємо сміття на пробіли
-			clipText = Regex.Replace(clipText, @"[^a-zA-Z0-9]", " ");
-            		// виявляємо 1 шаблон
-			firstEnglishBefore = PatternExtract(clipText,patternFirstEnglishBefore);
-			// виявляємо 2 шаблон
-			firstEnglishAfter = PatternExtract(clipText,patternFirstEnglishAfter);
-			// Формуємо результат та прибераємо дублі після конкатенації 
-            		clipText = RemoveDuplicates(firstEnglishBefore + firstEnglishAfter);
+			// перевірка ти відразу норм кори копіюїш?
+			if (clipText.Length != 18) {
+				// тут проблема!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				clipText = Regex.Replace(clipText, @"[^a-zA-Z0-9]", "");
+				// виявляємо 1 шаблон
+				firstEnglishBefore = PatternExtract(clipText,patternFirstEnglishBefore);
+				// виявляємо 2 шаблон
+				firstEnglishAfter = PatternExtract(clipText,patternFirstEnglishAfter);
+				// Формуємо результат: прибераємо дублі після конкатенації + додаємо пробіли 
+				clipText = InsertSpaces(RemoveDuplicates(firstEnglishBefore + firstEnglishAfter));
+			}
 			
-			// Переписує буфер обміну
-			Clipboard.SetText(clipText);
-			// Знаходить та активує вікно якщо воно звернуте 
-			var w = wnd.find(0, "Delta Monitor - Google Chrome", "Chrome_WidgetWin_1").Activate();
-			// Знаходить пошуковий рядок
-			var e = w.Elm["web:COMBOBOX", "Пошук", "@id=search-container-input"].Find(2).MouseClick();
-			// вставляє з буферу обміну 
-			keys.send("Ctrl+A Ctrl+V Enter");
+			Console.WriteLine(clipText);
 		}
 		// Функція для видалення дублікатів зі строки
 		static string RemoveDuplicates(string input) {
@@ -61,11 +60,64 @@ namespace CSLight
 		// отримаємо вміст шаблону з рядку
 		static string PatternExtract(string copyText, string pattern) {
 			Match match = Regex.Match(copyText, pattern);
-	            	if (match.Success)
-	            	{
-	                		return match.Value;
-	            	}
+            if (match.Success)
+            {
+                return match.Value;
+            }
 			 return string.Empty; // or return null, or any default value you prefer
+		}
+		//додаємо пробіли
+		static string InsertSpaces(string input)
+		{
+			/*
+			if (input.Length < 15)
+			{
+				return input; // If the input length is less than 15, no need to insert spaces
+			}
+			*/
+			// Insert spaces at the specified positions
+			input = input.Insert(input.Length - 5, " ");
+			input = input.Insert(input.Length - 11, " ");
+			input = input.Insert(input.Length - 14, " ");
+
+			return input;
+		}
+		
+		static string Transliterate(string text)
+		{
+			Dictionary<char, string> translitMap = new Dictionary<char, string>
+			{
+				{'А', "A"}, {'Б', "B"}, {'В', "V"}, {'Г', "H"}, {'Ґ', "G"},
+				{'Д', "D"}, {'Е', "E"}, {'Є', "Ye"}, {'Ж', "Zh"}, {'З', "Z"},
+				{'И', "Y"}, {'І', "I"}, {'Ї', "Yi"}, {'Й', "Y"}, {'К', "K"},
+				{'Л', "L"}, {'М', "M"}, {'Н', "N"}, {'О', "O"}, {'П', "P"},
+				{'Р', "R"}, {'С', "S"}, {'Т', "T"}, {'У', "U"}, {'Ф', "F"},
+				{'Х', "Kh"}, {'Ц', "Ts"}, {'Ч', "Ch"}, {'Ш', "Sh"}, {'Щ', "Shch"},
+				{'Ю', "Yu"}, {'Я', "Ya"},
+				{'а', "a"}, {'б', "b"}, {'в', "v"}, {'г', "h"}, {'ґ', "g"},
+				{'д', "d"}, {'е', "e"}, {'є', "ye"}, {'ж', "zh"}, {'з', "z"},
+				{'и', "y"}, {'і', "i"}, {'ї', "yi"}, {'й', "y"}, {'к', "k"},
+				{'л', "l"}, {'м', "m"}, {'н', "n"}, {'о', "o"}, {'п', "p"},
+				{'р', "r"}, {'с', "s"}, {'т', "t"}, {'у', "u"}, {'ф', "f"},
+				{'х', "kh"}, {'ц', "ts"}, {'ч', "ch"}, {'ш', "sh"}, {'щ', "shch"},
+				{'ю', "yu"}, {'я', "ya"}
+			};
+
+			StringBuilder result = new StringBuilder();
+
+			foreach (char c in text)
+			{
+				if (translitMap.ContainsKey(c))
+				{
+					result.Append(translitMap[c]);
+				}
+				else
+				{
+					result.Append(c);
+				}
+			}
+
+			return result.ToString();
 		}
 	}
 }
