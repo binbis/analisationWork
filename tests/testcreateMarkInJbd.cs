a770ac30c8b2ecab688b3275fc6102827be1d502
binbis <kingbuymem@gmail.com>
2024-08-03 04:06:36 +0300

    mines +- fork, speed need configurate

--- tests/testcreateMarkInJbd.cs
+++ tests/testcreateMarkInJbd.cs
@@ -1,4 +1,4 @@
-/* 01,08,2024_v1.1
+/* 01,08,2024_v1.3
 * в тестовому режимі
 функціонал заповнення мітки в тебе є
 тепер треба додати умови, розбити на класи, чи щось таке
@@ -12,14 +12,15 @@ using System.Windows.Forms;
 namespace CSLight {
 	class Program {
 		static void Main() {
-			/*
-			//opt.mouse.MoveSpeed = opt.key.KeySpeed = opt.key.TextSpeed = 20;
+			
+			opt.mouse.MoveSpeed = opt.key.KeySpeed = 25;
+			opt.key.TextSpeed = 20;
 			//виділяємо весь рядок
-			//keys.send("Shift+Space Space");
+			keys.send("Shift+Space*2");
 			//копіюємо код
 			keys.send("Ctrl+C");
 			// зчитуємо буфер обміну
-			string clipboardData = Clipboard.GetText();
+			string clipboardData = clipboard.copy();
 			
 			// Розділяємо рядок на частини
 			string[] parts = clipboardData.Split('\t');
@@ -36,6 +37,7 @@ namespace CSLight {
 			string establishedJbd = parts[24];
 			//приводжу дату до формату дельти
 			string dateDeltaFormat = dateJbd.Replace('.','/');
+			/*
 			// массив техніки
 			string[] machineryArray = {
 				"ББМ / МТ-ЛБ","Авто","Вантажівка"
@@ -45,72 +47,92 @@ namespace CSLight {
 			*/
 			// Знаходить та активує вікно якщо воно звернуте 
 			var w = wnd.find(0, "Delta Monitor - Google Chrome", "Chrome_WidgetWin_1").Activate();
-			wait.ms(200);
 			// поле шар
 			var layerWindow = w.Elm["web:GROUPING", prop: "@data-testid=select-layer"].Find(3);
 			layerWindow.Select();
+			wait.ms(100);
 			layerWindow.PostClickD(2);
+			wait.ms(100);
 			layerWindow.SendKeys("Ctrl+A","!11","Enter");
-			wait.ms(200);
+			
 			// поле назва
-			string markName = "ПТМ-3 (до 30.07)";
+			string markName = "ПТМ-3 до ("+ datePlasFourteen(dateJbd)+")";
 			var nameOfMarkWindow = w.Elm["web:TEXT", prop: new("@data-testid=T")].Find(3);
-			nameOfMarkWindow.PostClick();
+			nameOfMarkWindow.PostClick(2);
+			wait.ms(100);
 			nameOfMarkWindow.SendKeys("Ctrl+A","!"+markName);
-			wait.ms(200);
+			
+			// поле дата / час
+			var dateDeltaWindow = w.Elm["web:TEXT", prop: "@data-testid=W"].Find(3);
+			dateDeltaWindow.PostClick(2);
+			dateDeltaWindow.SendKeys("Ctrl+A","!"+dateDeltaFormat);
+			wait.ms(100);
+			var timeDeltaWindow = w.Elm["web:TEXT", prop: "@data-testid=W-time-input"].Find(1);
+			timeDeltaWindow.SendKeys("!"+timeJbd);
+			wait.ms(100);
+			nameOfMarkWindow.PostClick(2);
+			
 			// поле кількість
 			var numberOfnumberWindow = w.Elm["web:SPINBUTTON", prop: new("@data-testid=C", "@type=number")].Find(3);
 			numberOfnumberWindow.PostClick(1);
+			wait.ms(100);
 			numberOfnumberWindow.SendKeys("Ctrl+A", "!1");
-			wait.ms(200);
+			
 			// поле боєздатність
 			string fullaim = "Повні";
 			var combatCapabilityWindow = w.Elm["web:GROUPING", prop: "@data-testid=operational-condition-select"].Find(3);
 			combatCapabilityWindow.PostClickD(1);
+			wait.ms(100);
 			combatCapabilityWindow.SendKeys("","!"+fullaim, "Enter");
-			wait.ms(200);
+			
 			// ідетнифікація
 			string friendly = "дружній";
 			var identificationWindow = w.Elm["web:GROUPING", prop: "@data-testid=select-HO"].Find(3);
 			identificationWindow.PostClick(1);
+			wait.ms(100);
 			identificationWindow.SendKeys("Ctrl+A", "!"+friendly, "Enter");
-			wait.ms(200);
+			
 			// достовірність
 			var reliabilityWindow = w.Elm["web:RADIOBUTTON", "A", "@data-testid=reliability-key-A"].Find(3);
 			reliabilityWindow.PostClick(1);
+			wait.ms(100);
 			var certaintyWindow = w.Elm["web:RADIOBUTTON", "2", "@data-testid=reliability-key-2"].Find(3);
 			certaintyWindow.PostClick(1);
-			wait.ms(200);
+			
 			// тип джерела
 			string flyeye = "повітр";
 			var typeOfSourceWindow = w.Elm["web:GROUPING", prop: "@data-testid=AD", flags: EFFlags.HiddenToo].Find(1);
-			typeOfSourceWindow.PostClickD();
+			typeOfSourceWindow.PostClick(2);
+			wait.ms(100);
 			typeOfSourceWindow.SendKeys("Ctrl+A", "!"+flyeye, "Enter");
-			wait.ms(200);
+			wait.ms(100);
 			// завйваження штабу ід
-			string idPurchaseText = "id";
+			string idPurchaseText = idTargetJbd;
 			var idPurchaseWindow = w.Elm["web:TEXT", prop: "@data-testid=G", flags: EFFlags.HiddenToo].Find(1);
 			idPurchaseWindow.PostClick();
+			wait.ms(100);
 			idPurchaseWindow.SendKeys("Ctrl+A", "!"+idPurchaseText, "Enter");
-			wait.ms(200);
+			
 			// коментар
-			string commentContents = " - встановлена за допомогою ударного коптера ";
-			var commentWindow = w.Elm["web:TEXT", "Введіть значення", "@data-testid=comment-editing__textarea", EFFlags.HiddenToo].Find(1);
+			string commentContents = dateJbd + " " + timeJbd + " - встановлена за допомогою ударного коптера " + crewTeamJbd;
+			var commentWindow = w.Elm["web:TEXT", prop: "@data-testid=comment-editing__textarea"].Find(1);
 			commentWindow.PostClick();
 			commentWindow.SendKeys("Ctrl+A", "!"+commentContents);
 			
 			// кнопка коментаря
+			/*
 			var commentAsseptButton = w.Elm["web:BUTTON", prop: "@data-testid=comment-editing__button-save"].Find(1);
 			wait.ms(200);
 			commentAsseptButton.ScrollTo();
 			wait.ms(200);
 			commentAsseptButton.MouseClick();
+			*/
 		
 		}
 		static string datePlasFourteen(string date) {
 			// Перетворюємо рядок дати у DateTime
 			DateTime originalDate = DateTime.ParseExact(date, "dd.MM.yyyy", CultureInfo.InvariantCulture);
-			// Додаємо 14 днів
+			// Додаємо Х днів
 			DateTime newDate = originalDate.AddDays(60);
 			// Перетворюємо нову дату назад у рядок
 			//string newDateString = newDate.ToString("dd.MM.yyyy");
