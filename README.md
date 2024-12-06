fef00e012bf7e8a0bb9027eb33ddf6a77bab0a68
binbis <kingbuymem@gmail.com>
2024-12-06 21:51:46 +0200

    new crew-id, change names, rewriter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 HotKeys:
 
-	hk["Alt+X"] = o => script.run(@"\analisationWork-main\main\fastCopDeltaOPBS.cs");
-	hk["Alt+Z"] = o => script.run(@"\analisationWork-main\main\fillMarkOPBS.cs");
-	hk["Ctrl+Alt+T"] = o => script.run(@"\analisationWork-main\main\ecipashCreateFastOPBS.cs");	
-	hk["Alt+Space"] = o => script.run(@"\analisationWork-main\main\renameFormatFilesOPBS.cs");
+	hk["Alt+X"] = o => script.run(@"\analisationWork-main\main\fastCopDelta.cs");
+	hk["Alt+Z"] = o => script.run(@"\analisationWork-main\main\fillMark.cs");
+	hk["Ctrl+Alt+T"] = o => script.run(@"\analisationWork-main\main\ecipashCreateFast.cs");	
+	hk["Alt+Space"] = o => script.run(@"\analisationWork-main\main\renameFormatFiles.cs");
--- tests/renameFormatFiles.cs
+++ tests/renameFormatFiles.cs
@@ -1,10 +1,19 @@
-/*/ c \analisationWork-main\globalClass\Bisbin.cs; /*/
-/* 25,11,2024
+/*/ c \analisationWork\globalClass\Bisbin.cs; /*/
+/* 07,12,2024
+
+!!renameAll!!:
 проєкт - переіменування файлів в папці
 
 + знайти перейменувати вміст та відкрити папку
 + обійти екіпаж crewTeamJbd з кінця та обрізати усе до )
 + розділення на готове речення в буфер обміну
+
+!!recordPathToMemory!!:
+на основі жбд створує папку в папці з ієрархією
+folde -> нава екіпажу -> ід цілі
+та відкриває її
+
+потім зможе щей перейменовувати, до ід цілі номер вильоту приписувати інший вже до існуючого
 
 */
 
@@ -18,29 +27,65 @@ namespace CSLight {
 			opt.key.KeySpeed = 20;
 			opt.key.TextSpeed = 20;
 			
-			keys.send("Shift+Space*2"); //виділяємо весь рядок
-			wait.ms(100);
 			keys.send("Ctrl+C"); // копіюємо код
 			wait.ms(100);
 			string clipboardData = clipboard.copy(); // зчитуємо буфер обміну
-			string[] parts = clipboardData.Split('\t'); // Розділяємо рядок на частини
 			
-			// вікно діалогу
-			var b = new wpfBuilder("Window").WinSize(600);
+			var bMain = new wpfBuilder("Window").WinSize(600); // основне вікно
+			var b = bMain; // 
+			b.Row(-1).Add(out TabControl tc).Height(250..); // підтримка табуляції + фіксована висота
+			b.R.AddOkCancel(apply: "_Apply").Font(size: 14, bold: false); // додаткова кнопка до ok cansel
+			b.Window.Topmost = true; // поверх усіх вікон
+			// 
+			wpfBuilder _Page(string name, WBPanelType panelType = WBPanelType.Grid) {
+				var tp = new TabItem { Header = name };
+				tc.Items.Add(tp);
+				return new wpfBuilder(tp, panelType);
+			}
+			
+			var b1 = b = _Page("Скрипти");
 			//Brush
 			b.Font(size: 17, bold: true);
 			b.Brush(Brushes.DarkGray);
 			// insider
 			b.R.AddButton("1. Готове речення в буфер обміну", 2).Brush(Brushes.LightCoral);
-			b.R.AddButton("2. Перейменування в папці Бамбас", 1).Brush(Brushes.LightSalmon);
-			b.R.AddButton("3. Перейменування в папці Уголь", 3).Brush(Brushes.LightGoldenrodYellow);
-			b.R.AddButton("4. Перейменування в папці Уголь-Суджа", 4).Brush(Brushes.LightGoldenrodYellow);
-			b.R.AddOkCancel().Font(size: 14, bold: false);
-			b.Window.Topmost = true;
+			b.R.AddButton("2. Створти папку екіпаж -> ід", 5).Brush(Brushes.LightCyan);
+			b.R.AddButton("3. Перейменування в папці Бамбас", 1).Brush(Brushes.LightSalmon);
+			b.R.AddButton("4. Перейменування в папці Уголь", 3).Brush(Brushes.LightGoldenrodYellow);
+			b.R.AddButton("5. Перейменування в папці Уголь-Суджа", 4).Brush(Brushes.LightGoldenrodYellow);
 			b.End();
-			// show dialog. Exit if closed not with the OK button.
+			
+			var b2 = b = _Page("Технічне налаштування");
+			//Brush
+			b.Font(size: 16, bold: true);
+			b.Brush(Brushes.DarkGray);
+			// insider
+			b.R.Add(out Label _, "Шлях до папки де буде створено папки в папках").AlignContent(HorizontalAlignment.Center);
+			b.R.Add(out TextBox rememberedTextPath).Font(size: 14, bold: false).Size(400, 40).AddButton("Запам'ятати", 99).Brush(Brushes.LightCoral).Width(125);
+			b.End();
+			b = bMain.End();
 			if (!b.ShowDialog()) return;
 			
+			int dialogButtonResOne = b1.ResultButton; // значення нажатої кнопки
+			int dialogButtonResTwo = b2.ResultButton; // значення нажатої кнопки
+			string rememberPath = @"C:\Users\User-PM\Documents\LibreAutomate\Main\files\analisationWork\customTemp\pathes.txt"; // шлях до файлу зі збереженим шляхом
+			
+			// основа
+			if (dialogButtonResOne != 5 && dialogButtonResOne != 99) {
+				renameAll(clipboardData, dialogButtonResOne);
+			}
+			
+			if (dialogButtonResOne == 5) {
+				PathWeaver(clipboardData, rememberPath);
+			}
+			
+			if (dialogButtonResTwo == 99) {
+				recordPathToMemory(rememberedTextPath.Text, rememberPath);
+			}
+		}
+		// функ перейменування 
+		static void renameAll(string clipboardData, int dialogButtonRes) {
+			string[] parts = clipboardData.Split('\t'); // Розділяємо рядок на частини
 			// Присвоюємо змінним відповідні значення
 			string dateJbd = getDDnMM(parts[0]); // 27.07.2024 - 27.07
 			string timeJbd = parts[1].Replace(":", "."); // 00:40 - 00.40
@@ -57,7 +102,7 @@ namespace CSLight {
 			string pathCoalFolderDon = @"\\Sng-2\аеророзвідка\combatlog\Vugl_Combat_Log"; //
 			string pathCoalFolderSsy = @"\\Sng-2\аеророзвідка\combatlog\Sumy_Combat_Log"; // 
 			
-			if (b.ResultButton == 2) {
+			if (dialogButtonRes == 2) {
 				if (crewTeamJbd.Contains("FPV")) {
 					Clipboard.SetText($"{dateJbd} {timeJbd} {crewTeamJbd} В{numberOFlying} - {establishedJbd.ToLower()}");
 					return;
@@ -69,9 +114,9 @@ namespace CSLight {
 			
 			if (messageId.Length > 3) {
 				string pathFilesOpen = string.Empty;
-				if (b.ResultButton == 3) {
+				if (dialogButtonRes == 3) {
 					pathFilesOpen = Path.Combine(pathCoalFolderDon, messageId);
-				} else if (b.ResultButton == 4) {
+				} else if (dialogButtonRes == 4) {
 					pathFilesOpen = Path.Combine(pathCoalFolderSsy, messageId);
 				} else {
 					pathFilesOpen = Path.Combine(pathDonbasFolder, messageId);
@@ -115,12 +160,44 @@ namespace CSLight {
 					wait.ms(200);
 					//Console.WriteLine(filesInDirectory[i] + "\n");
 				}
-				if (b.ResultButton == 1 || b.ResultButton == 3 || b.ResultButton == 4) {
+				if (dialogButtonRes == 1 || dialogButtonRes == 3 || dialogButtonRes == 4) {
 					// відкриття папки за шляхом
 					Process.Start("explorer.exe", pathFilesOpen);
 				}
 			}
+		}
+		// папки з папками
+		static void PathWeaver(string clipboardData, string rememberPath) {
+			string pathWithRemebers = File.ReadAllText(rememberPath);
 			
+			string[] parts = clipboardData.Split('\t'); // Розділяємо рядок на частини
+			string flyNumber = parts[3]; // номер вильоту
+			string crewName = parts[4]; // імя екіпажу
+			string aidId = parts[9]; // ід цілі
+			
+			string partFolderByCrew = $"{crewName}"; // заготовка екіпажа
+			string partFolderByFlyNumberAndId = $"({flyNumber}) {aidId}"; // заготовка ід
+			
+			string pathToFolderByCrew = pathname.combine(pathWithRemebers,partFolderByCrew); // збирає шляхи папки екіпаж
+			bool f = filesystem.createDirectory(pathToFolderByCrew); // пробую створити папку
+			//перевірка
+			if (!f) {
+				Console.WriteLine($"папка {crewName} вже існує");
+			}
+			wait.ms(400);
+			string pathToFolderById = pathname.combine(pathToFolderByCrew,partFolderByFlyNumberAndId); // збирає шляхи папки ід
+			f = filesystem.createDirectory(pathToFolderById); // пробую створити папку
+				//перевірка
+			if (!f) {
+				Console.WriteLine($"папка {partFolderByFlyNumberAndId} в {partFolderByCrew} папці вже існує");
+			}
+			//Console.WriteLine($"{}");
+			Process.Start("explorer.exe", pathToFolderById); // відкриваємо папку
+		}
+		// запам'ятовувалка
+		static void recordPathToMemory(string rememberedTextPath, string rememberPath) {
+			File.WriteAllText(rememberPath, rememberedTextPath); //creates new file or overwrites existing file
+			osdText.showTransparentText("Успішно перезеписав шлях");
 		}
 		// формат для перейменування відео 27.07.2024 - 27.07
 		static string getDDnMM(string str) {
@@ -152,6 +229,7 @@ namespace CSLight {
 			}
 			return establishedJbd;
 		}
+		
 	}
 }
 
